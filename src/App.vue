46fc96710d0b6432f0a97acb4cedc188b5452d59
daykrois <saiyanqlwy@foxmail.com>
2024-02-29 23:03:37 +0800

    all active completed按钮效果

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,55 @@
 <script>
 export default {
-
+  data () {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: '测试',
+          completed: false
+        },
+        {
+          id: 2,
+          title: '测试1',
+          completed: true
+        }  
+      ],
+	  visibility: 'all'
+    }
+  },
+  methods: {
+    toggleAll (e) {
+      this.todos.forEach(todo => todo.completed = e.target.checked)
+    },
+    onHashChange(){
+		const hash = window.location.hash.replace(/#\/?/,'')
+		if (['all','active','completed'].includes(hash)) {
+			this.visibility = hash
+		}else{
+			window.location.hash = ''
+			this.visibility = 'all'
+		}
+	}
+  },
+  computed: {
+	filteredTodos(){
+		switch(this.visibility){
+			case 'all':
+				return this.todos
+			case 'active':
+				return this.todos.filter(todo => !todo.completed)
+			case 'completed':
+				return this.todos.filter(todo => todo.completed)
+		}
+	},
+	remaining() {
+		return this.todos.filter(todo => !todo.completed).length
+	}
+  },
+  mounted(){
+    window.addEventListener('hashchange',this.onHashChange)
+	this.onHashChange()
+  }
 }
 </script>
 
@@ -12,43 +61,39 @@ export default {
 			</header>
 			<!-- This section should be hidden by default and shown when there are todos -->
 			<section class="main">
-				<input id="toggle-all" class="toggle-all" type="checkbox">
+				<input id="toggle-all" class="toggle-all" type="checkbox" @click="toggleAll">
 				<label for="toggle-all">Mark all as complete</label>
 				<ul class="todo-list">
 					<!-- These are here just to show the structure of the list items -->
 					<!-- List items should get the class `editing` when editing and `completed` when marked as completed -->
-					<li class="completed">
+					<li
+            v-for="todo in filteredTodos"
+            :key="todo.id" 
+            :class="{completed: todo.completed}">
 						<div class="view">
-							<input class="toggle" type="checkbox" checked>
-							<label>Taste JavaScript</label>
+							<input class="toggle" type="checkbox" v-model="todo.completed">
+							<label v-text="todo.title"></label>
 							<button class="destroy"></button>
 						</div>
 						<input class="edit" value="Create a TodoMVC template">
 					</li>
-					<li>
-						<div class="view">
-							<input class="toggle" type="checkbox">
-							<label>Buy a unicorn</label>
-							<button class="destroy"></button>
-						</div>
-						<input class="edit" value="Rule the web">
-					</li>
+					
 				</ul>
 			</section>
 			<!-- This footer should be hidden by default and shown when there are todos -->
 			<footer class="footer">
 				<!-- This should be `0 items left` by default -->
-				<span class="todo-count"><strong>0</strong> item left</span>
+				<span class="todo-count"><strong>{{ remaining }}</strong> item left</span>
 				<!-- Remove this if you don't implement routing -->
 				<ul class="filters">
 					<li>
-						<a class="selected" href="#/">All</a>
+						<a :class="{selected: visibility === 'all'}" href="#/all">All</a>
 					</li>
 					<li>
-						<a href="#/active">Active</a>
+						<a :class="{selected: visibility === 'active'}" href="#/active">Active</a>
 					</li>
 					<li>
-						<a href="#/completed">Completed</a>
+						<a :class="{selected: visibility === 'completed'}" href="#/completed">Completed</a>
 					</li>
 				</ul>
 				<!-- Hidden if no completed items are left ↓ -->
